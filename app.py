893a8ce7f8fb1536820f9d799ce2c474d3e8d3ad
smart75s <smart75s@gmail.com>
2026-05-04 00:08:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.title("مرحباً بك!")
+st.write("إذا كنت ترى هذه الرسالة، فهذا يعني أن التطبيق يعمل بنجاح.")
+
+# اختبار رفع ملف بسيط
+file = st.file_uploader("جرب رفع ملف هنا")
+if file:
+    st.success("تم رفع الملف بنجاح!")
